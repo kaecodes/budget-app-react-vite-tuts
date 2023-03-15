fa909a5bd5d132777ea3218af35b2003d1db5d3a
Kaela Nguyen <kaedtn96@gmail.com>
2023-03-14 22:29:44 -0700

    Create first route and loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import {
+  createBrowserRouter, 
+  RouterProvider 
+} from "react-router-dom"; 
+
+// Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />, 
+    loader: dashboardLoader
+  }, 
+  {
+    path: "/about",
+    element: <h1>About</h1>
+  }
+])
+
 function App() {
  
-
   return (
     <div className="App">
-      
+      <RouterProvider router={router} />
     </div>
   )
 }
